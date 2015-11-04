97454672ab152ec5116dc967e5ce386a6ec99a67
Eric Pinheiro <ericson.pp@gmail.com>
2015-11-04 21:08:07 -0200

    Classe conexao atualizada

--- src/PERS/Conexao.java
+++ src/PERS/Conexao.java
@@ -1,10 +1,24 @@
 package PERS;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author Jean_Henrique_Ferreira
- * Andressa
- * teste
+ * @author Andressa
+ * @author Eric Pinheiro
  */
 public class Conexao {
-    
+   private static String url = "jdbc:postgresql://localhost:5432/NOME_BASE_DE_DADOS"; // alterar depois de criar o db
+        private static String user = "postgres"; //usuario padrao
+        private static String senha = "masterkey"; //colcoar a senha que foi definida no db
+        
+        public Connection getConnection(){
+            try {
+                return DriverManager.getConnection(url, user, senha);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }  
 }
